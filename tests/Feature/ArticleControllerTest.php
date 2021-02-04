a7b5be619cb88d2fc76952a4d17620c83f27e9e5
johnmuthua <caveomuthama@gmail.com>
2021-02-04 16:25:03 +0300

    test:everyone can view individual articles

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Article;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -16,6 +17,22 @@ class ArticleControllerTest extends TestCase
     public function it_allows_anyone_to_see_list_of_all_articles()
     {
         $response = $this->get(route('get_all_articles'));
-        $response->assertSuccessful();
+
+        $response->assertViewIs('articles.index');
+        $response->assertViewHas('articles');
+    }
+
+    /**
+     * @test
+     */
+    public function it_allows_everone_to_view_individual_articles()
+    {
+        $article = Article::get()->random();
+        $response = $this->get(route('view_article', ['id' => $article->id]));
+
+        $response->assertViewIs('articles.view');
+        $response->assertViewHas('article');
+        $returnedArticle = $response->original->article;
+        $this->assertEquals($article->id, $returnedArticle->id, "The returned article is different from the one we requested");
     }
 }
